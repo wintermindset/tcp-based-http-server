8b30258c1f357497b8addf715e5c058df97ff18d
wintermindset <wintermindset@yandex.ru>
2026-03-11 13:12:04 -0700

    update App.java
    Closes #13

--- src/main/java/com/wintermindset/App.java
+++ src/main/java/com/wintermindset/App.java
@@ -1,11 +1,20 @@
 package com.wintermindset;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import com.wintermindset.config.ConfigReader;
+import com.wintermindset.config.ServerConfig;
+import com.wintermindset.core.Server;
+
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    
+    public static void main(String[] args) {
+        try {
+            ServerConfig config = ConfigReader.loadFromResources("server-config.yaml");
+            Server server = new Server(config);
+            server.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
